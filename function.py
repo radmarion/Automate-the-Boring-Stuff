9f9d2af44641a89859ce68a447fa36c843976eb9
Alake Oluwatunmise <54968946+radmarion@users.noreply.github.com>
2022-06-08 14:26:31 +0000

    Docstring about None Value Data type and the print Function

--- function.py
+++ function.py
@@ -10,7 +10,10 @@ def plusOne(number):
 
 Newnumber = plusOne(4)
 print(Newnumber)
+'''Print fucntion returns a NONE(Data type) value i.e neither true nor false, The text or variable being displayed is a side effect of the print function'''
 
 print('Hello', end="_"); print('World')
 
 print('Well','I am','a boy', sep = '__')
+
+''' Global and Local Function Scoped'''
